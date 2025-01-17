fa8121851285127778d93ba59e65f12462eacb7c
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-17 07:35:17 -0300

    setup-transpilation of the files generated so far.

---
+++ backend/dist/database/config/database.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const config = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '123456',
+  database: process.env.DB_NAME || 'banking_api',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT || '3306'),
+  dialect: 'mysql',
+  logging: false
+};
+module.exports = config;
