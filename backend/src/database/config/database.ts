c6df490ad49ddf2f89b2083d2740f87de6eb7433
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-17 07:16:01 -0300

    fix-setup-add database password

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -4,7 +4,7 @@ import { Options } from "sequelize";
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || '123456',
   database: process.env.DB_NAME || 'banking_api',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT || '3306'),
