ed1a99adf1a633d7baba9ac709621f2dcf70ee5b
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-18 18:18:57 -0300

    chore-services-Client-determines type for client search and return null if not found.

--- backend/src/services/ClientsService.ts
+++ backend/src/services/ClientsService.ts
@@ -29,13 +29,16 @@ class ClientsService {
 
   public getClientById = async (id: number): Promise<IClient | null> => {
     try {
-      const client = await ClientModel.findByPk(id, {
+      const client: IClient | null = await ClientModel.findByPk(id, {
         attributes: { exclude: ['password'] },
         include: [
-          { model: PixModel, as: "creditor", attributes: ['id', 'value', 'message', 'status'] },
-          { model: PixModel, as: "payer", attributes: ['id', 'value', 'message', 'status'] },
+          { model: PixModel, as: "receivedPix", attributes: ['id', 'value', 'message', 'status', 'payerClientId'] },
+          { model: PixModel, as: "paidPix", attributes: ['id', 'value', 'message', 'status', 'creditedClientId'] },
         ],
       });
+
+      if (!client) return null;
+
       return client;
     } catch (error) {
       throw new Error((error as Error).message);
