88b8f4b5090473af0ae54a6475430512cb54c137
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-19 17:13:50 -0300

    setup-initial setup after the creation of a react app and tailwind configuration.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    <div>
+      <header>
+        <div className="bg-yellow-500">
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p className="text-yellow-700">Este texto deve ter uma cor entre laranja e marrom</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
