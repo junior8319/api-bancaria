99df62f1a123ba8523e32448ca971e608a61585f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-19 12:44:29 -0300

    fix-interfaces-ClientData-defines the interface IClientData to can clean data generated by the model when registering a new client.

--- backend/src/interfaces/ClientData.ts
+++ backend/src/interfaces/ClientData.ts
@@ -2,9 +2,22 @@ export default interface IClient {
   id: number
   name: string;
   cpf: string;
-  password: string;
+  password?: string;
 
-  token?: {
-    data?: string;
-  };
+  token?: string | null;
 }
+
+export interface IClientData {
+  dataValues: {
+    id: number
+    name: string;
+    cpf: string;
+    password?: string;
+  };
+  _previousDataValues?: object;
+  uniqno?: number;
+  _changed?: object;
+  _options?: object;
+  isNewRecord?: boolean;
+  token?: string | null;
+}    
