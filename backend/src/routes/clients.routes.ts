b77e40e860b7874cf09f4820c9e3fb930db0ff73
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-19 13:47:54 -0300

    feat-login-routes-set post route '/login'.

--- backend/src/routes/clients.routes.ts
+++ backend/src/routes/clients.routes.ts
@@ -6,5 +6,6 @@ const clientsRouter = Router();
 clientsRouter.get("/clients", ClientsController.getAllClients);
 clientsRouter.get("/clients/:id", ClientsController.getClientById);
 clientsRouter.post("/clients", ClientsController.createClient);
+clientsRouter.post("/login", ClientsController.login);
 
 export default clientsRouter;
