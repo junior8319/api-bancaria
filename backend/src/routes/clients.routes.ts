1b30db7ff7fa4b5849e68f839c92a6ddbaafa637
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-19 15:01:50 -0300

    feat-Pix-middlewares-routes- adds the token validation to the route to register a pix.

--- backend/src/routes/clients.routes.ts
+++ backend/src/routes/clients.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import ClientsController from "../controllers/ClientsController";
 import PixController from "../controllers/PixController";
+import validateToken from "../middlewares/validateToken";
 
 const clientsRouter = Router();
 
@@ -8,6 +9,6 @@ clientsRouter.get("/clients", ClientsController.getAllClients);
 clientsRouter.get("/clients/:id", ClientsController.getClientById);
 clientsRouter.post("/clients", ClientsController.createClient);
 clientsRouter.post("/login", ClientsController.login);
-clientsRouter.post("/pix", PixController.createPix);
+clientsRouter.post("/pix", validateToken, PixController.createPix);
 
 export default clientsRouter;
