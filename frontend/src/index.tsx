3d9fb28d19328401d330e1dbd98221e044e755d8
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-20 18:21:41 -0300

    feat--context-provides the context to App in the file inex.tsx.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import App from './App.tsx';
+import ClientsProvider from './context/ClientsProvider.tsx';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ClientsProvider>
+      <App />
+    </ClientsProvider>
   </React.StrictMode>
 );
