4c2d161ee73a491588abb502f6e00bd566ddf1c2
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-26 18:18:07 -0300

    chore-frontend-styles: login component styling.

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -10,24 +10,55 @@ const Login = () => {
   const sendLoginRequest = context?.sendLoginRequest;
 
   return (
-    <div>
-      <h1>API de Pix</h1>
-      <form>
-        <p className="text-center">
-          <label htmlFor="cpf">CPF</label>
-          <input type="text" id="cpf" name="cpf" value={cpf} onChange={handleInputChange} />
-        </p>
+    <main className="flex items-center justify-center min-h-screen">
+      <section className="w-full max-w-sm bg-slate-600 shadow-lg rounded-2xl p-6">
+        <h1 className="text-2xl font-bold text-center mb-6">API de Pix</h1>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="cpf"
+              className="block text-sm font-medium text-green-500"
+            >
+              CPF
+            </label>
+            <input
+              type="text"
+              id="cpf"
+              name="cpf"
+              value={cpf}
+              onChange={handleInputChange}
+              className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-800"
+            />
+          </div>
+          
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-green-500"
+            >
+              Senha
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={handleInputChange}
+              className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-800"
+            />
+          </div>
 
-        <p className="text-center">
-          <label htmlFor="password">Senha</label>
-          <input type="password" id="password" name="password" value={password} onChange={handleInputChange} />
-        </p>
-
-        <p className="text-center">
-          <button onClick={sendLoginRequest}>Login</button>
-        </p>
-      </form>
-    </div>
+          <div>
+            <button
+              onClick={sendLoginRequest}
+              className="w-full px-4 py-2 text-white bg-blue-600 border border-transparent rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 hover:bg-transparent hover:border-[#1ED760] hover:text-[#1ED760] transition duration-500"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </section>
+    </main>
   );
 };
 
