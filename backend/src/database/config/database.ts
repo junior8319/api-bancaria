f77d502dabfcde5d1b2cc579748f7300378cc8ee
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2025-01-31 07:19:41 -0300

    fix-database-config: fix environment variables of database's configuration.

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -1,12 +1,11 @@
 import 'dotenv/config';
 import { Options } from "sequelize";
 
-
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '123456',
-  database: process.env.DB_NAME || 'banking_api',
-  host: process.env.DB_HOST || 'localhost',
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD || '123456',
+  database: process.env.MYSQL_DATABASE || 'banking_api',
+  host: process.env.DB_HOST || 'db',
   port: Number(process.env.DB_PORT || '3306'),
   dialect: 'mysql',
   logging: false
